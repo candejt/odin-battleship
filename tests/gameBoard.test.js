b6d54ed030934b586b9bf06ff06868d77f052ab2
candejt <139970803+candejt@users.noreply.github.com>
2026-06-15 19:50:12 +0200

    test: write tests for Game Board (create gameboard, place ship, receive attack, all sunk)

--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -1,1 +1,71 @@
+import { expect } from "chai";
 import GameBoard from "../src/gameBoard.js";
+import Ship from "../src/ship.js";
+
+test("creates a game board", () => {
+  const board = new GameBoard();
+  expect(board).toBeDefined();
+});
+
+test("places a ship on the board", () => {
+  const board = new GameBoard();
+  const ship = new Ship(3);
+  board.placeShip(ship, [0, 0], "horizontal");
+
+  expect(board.ships).toContain(ship);
+});
+
+test("receiveAttack hits a coordinate with a ship", () => {
+  const board = new GameBoard();
+  const ship = new Ship();
+  board.placeShip(ship, [0, 0], "horizontal");
+  board.receiveAttack([0, 0]);
+
+  expect(ship.hits).toBe(1);
+});
+
+test("receiveAttack hits a coordinate with no ship", () => {
+  const board = new GameBoard();
+  const ship = new Ship();
+  board.placeShip(ship, [0, 0], "horizontal");
+  board.receiveAttack([5, 5]);
+
+  expect(board.misses).toContain([5, 5]);
+});
+
+test("receiveAttack does not hit twice on the same coordinate", () => {
+  const board = new GameBoard();
+  const ship = new Ship();
+  board.placeShip(ship, [0, 0], "horizontal");
+  board.receiveAttack([0, 0]);
+  board.receiveAttack([0, 0]);
+
+  expect(ship.hits).toBe(1);
+});
+
+test("allSunk returns true when all ships are sunk", () => {
+  const board = new GameBoard();
+  const ship1 = new Ship(1);
+  const ship2 = new Ship(1);
+
+  board.placeShip(ship1, [0, 0], "horizontal");
+  board.placeShip(ship2, [1, 0], "horizontal");
+
+  board.receiveAttack([0, 0]);
+  board.receiveAttack([1, 0]);
+
+  expect(board.allSunk()).toBe(true);
+});
+
+test("allSunk returns false when not all ships are sunk", () => {
+  const board = new GameBoard();
+  const ship1 = new Ship(1);
+  const ship2 = new Ship(1);
+
+  board.placeShip(ship1, [0, 0], "horizontal");
+  board.placeShip(ship2, [1, 0], "horizontal");
+
+  board.receiveAttack([0, 0]);
+
+  expect(board.allSunk()).toBe(false);
+});
