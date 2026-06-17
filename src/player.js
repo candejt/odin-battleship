5def750af68e6e95b484c62f0483071fba033510
candejt <139970803+candejt@users.noreply.github.com>
2026-06-17 19:15:04 +0200

    Implement Player/Computer logic and add Player/CPU tests (attack, repeat-check, coord validation)

--- src/player.js
+++ src/player.js
@@ -1,4 +1,37 @@
 import GameBoard from "./gameBoard.js";
-export default class Player {
-  constructor() {}
+class Player {
+  attack(coord, enemyBoard) {
+    enemyBoard.receiveAttack(coord);
+  }
 }
+
+class Computer {
+  constructor(){
+    this.attacked = [];
+  }
+
+  randomAttack(enemyBoard) {
+    let coord;
+    
+    do {
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10);
+      coord = [x,y];
+    } while (this.alreadyAttacked(coord));
+
+    this.attacked.push(coord);
+    enemyBoard.receiveAttack(coord);
+  }
+
+  alreadyAttacked(coord){
+    for (let i = 0; i < this.attacked.length; i++){
+      const prev = this.attacked[i];
+      if (prev[0] === coord[0] && prev[1] === coord[1]){
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+export { Player, Computer };
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,1 +1,59 @@
-import Player from "../src/player.js";
+import GameBoard from "../src/gameBoard.js";
+import { Player, Computer } from "../src/player.js";
+
+test("player attacks enemy board", () => {
+  const player = new Player();
+  const enemyBoard = {
+    receiveAttack(coord) {
+      this.lastAttack = coord;
+    },
+  };
+  player.attack([3, 4], enemyBoard);
+
+  expect(enemyBoard.lastAttack).toEqual([3, 4]);
+});
+
+test("computer attacks enemy board", () => {
+  const cpu = new Computer();
+  const enemyBoard = {
+    receiveAttack(coord) {
+      this.lastAttack = coord;
+    },
+  };
+  cpu.randomAttack(enemyBoard);
+  expect(enemyBoard.lastAttack).not.toBeNull();
+  expect(enemyBoard.lastAttack.length).toBe(2);
+});
+
+test("computer does not repeat attacks", () => {
+  const cpu = new Computer();
+  const enemyBoard = {
+    attacks: [],
+    receiveAttack(coord) {
+      this.attacks.push(coord);
+    },
+  };
+  cpu.randomAttack(enemyBoard);
+  cpu.randomAttack(enemyBoard);
+
+  const [first, second] = enemyBoard.attacks;
+
+  expect(first).not.toEqual(second);
+});
+
+test("computer generates valid coord", () => {
+  const cpu = new Computer();
+  const enemyBoard = {
+    receiveAttack(coord) {
+      this.lastAttack = coord;
+    },
+  };
+  cpu.randomAttack(enemyBoard);
+
+  const [x, y] = enemyBoard.lastAttack;
+
+  expect(x).toBeGreaterThanOrEqual(0);
+  expect(x).toBeLessThan(10);
+  expect(y).toBeGreaterThanOrEqual(0);
+  expect(y).toBeLessThan(10);
+});
